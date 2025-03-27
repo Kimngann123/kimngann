249e8e11713cc7ec65a9364279f0fa1245400c9e
Kimngann123 <hongockimngan2810@gmail.com>
2025-03-27 23:28:53 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 function login()
-{
-  let u = us.value
-  let p = pw.value
-  if(u == "kimngan" && p == "200907")
-  {
-    window.open("home.html")
-    document.getElementById(compiler).style.display="block"
-  }
+{   
+    let a = username.value
+    let b = password.value
+    if (a == "kimngan" && b == "200907")
+    {   
+        window.open("home.html")
+        document.getElementById(compiler).style.display="block";
+    }
+    else 
+        document.write("Không hợp lệ")
 }
